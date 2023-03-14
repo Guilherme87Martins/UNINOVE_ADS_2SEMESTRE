eb133a2c71bb9d8739c3cfe6f85f2a12ffc71fb5
Guilherme <guilherme.cavalcanti87@gmail.com>
2023-03-14 19:55:40 -0300

    exercício

---
+++ Primeiro_projeto/src/Matematica.java
@@ -0,0 +1,35 @@
+public class Matematica {
+    
+    //  Atributos locais
+    double a, b;
+    
+    //  Método Construtor
+    public Matematica(double num_a, double num_b){
+        a = num_a;
+        b = num_b;
+    }
+    
+    //  Método soma
+    public double somar(){
+        return (a + b);
+    }
+    
+     //  Método soma
+    public double subtrair(){
+        return (a - b);
+    }
+    
+     //  Método soma
+    public double multiplicar(){
+        return (a * b);
+    }
+    
+     //  Método soma
+    public double dividir(){
+        return (a / b);
+    }
+    
+    public double calcular_IMC(){
+        return (a / (b * b) );
+    }
+}
---
+++ Primeiro_projeto/src/Primeiro_projeto.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class Primeiro_projeto {
+    public static void main(String[] args) {
+        System.out.println("Calculadora de inteiros");
+        //   variaveis
+        double a, b;
+        
+        Scanner ler = new Scanner(System.in);
+        // 1º Valores 
+        System.out.print("Primeiro valor: ");
+        a = ler.nextDouble();
+        
+        System.out.print("Segundo valor: ");
+        b = ler.nextDouble();
+        
+        //  Instanciar a classe Matematica
+        Matematica calcular = new Matematica(a, b);
+        
+        
+        System.out.print("\n----------------------\n");
+        System.out.print("\nA soma dos valores é: " + calcular.somar() );
+        System.out.print("\nA subtração dos valores é: " + calcular.subtrair());
+        System.out.print("\nA multiplicação dos valores é: " + calcular.multiplicar());
+        System.out.print("\nA divisão dos valores é: " + calcular.dividir());             
+    
+
+        System.out.print("\n----------------------\n");
+        System.out.print("Informe o PESO: ");
+        a = ler.nextDouble();
+        
+        System.out.print("Informe a ALTURA: ");
+        b = ler.nextDouble();
+        //  Instanciar a classe Matematica
+        Matematica imc = new Matematica(a, b);
+        
+        System.out.print("\nO valor do IMC é =   " + imc.calcular_IMC() );
+        
+        
+    }
+}
